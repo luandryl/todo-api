50ad1db94cd67856c8f0297ba45ca1fbbfbc6436
Luan <luan.andryl@gmail.com>
2019-02-18 04:42:54 -0300

    task_list by user

--- src/controllers/Todo.Controller.js
+++ src/controllers/Todo.Controller.js
@@ -5,4 +5,22 @@ export default class TodoController extends BaseController {
     constructor() {
         super(TodoModel)
     }
+
+    byUser (req, res) {
+        let query = {
+            _user: req.params._user
+        }
+        new TodoModel().getByField(query).then((data) => {
+            console.log(data)
+            if(data) {
+                res.send(data);
+                res.status(200)
+                res.end()
+            }
+        }).catch(err => {
+            res.json(err);
+            res.status(400);
+            res.end();
+        })
+    }
 }
--- src/routes/Todo.Router.js
+++ src/routes/Todo.Router.js
@@ -6,25 +6,33 @@ let router = express.Router()
 
 let todo = new TodoController()
 
+import passport from 'passport'
+const protect = passport.authenticate('jwt', {
+	session: false
+})
 
 
-router.get('/', (req, res) => {
+router.get('/', protect, (req, res) => {
     todo.getAll(req, res);
 })
 
-router.post('/', (req, res) => {
+router.post('/', protect, (req, res) => {
     todo.save(req, res)
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', protect, (req, res) => {
     todo.getById(req, res)
 })
  
-router.put('/:_id', (req, res) => {
+router.put('/:_id', protect, (req, res) => {
     todo.updateById(req, res)
 })
 
-router.delete('/:id', (req, res) => {
+router.get('/task_list/:_user', protect, (req, res) => {
+    todo.byUser(req, res)
+})
+
+router.delete('/:id', protect, (req, res) => {
     todo.removeById(req, res)
 })
 
